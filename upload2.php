b22366d3e52210cd59b6a0b941e4b4b1c1f77d10
王秋石 <qiushi@reco.cn>
2013-04-11 11:02:34 +0800

    额

--- upload2.php
+++ upload2.php
@@ -1,7 +1,8 @@
 <?php
 $x = explode(".",$_FILES['file']['name']);
-$name = $x[0];
-$ext = $x[1];
-move_uploaded_file($_FILES['file']['tmp_name'],$name.'_compressed'.'.'.$ext);
+$ext = array_pop($x);
+$name = implode('.',$x);
+
+move_uploaded_file($_FILES['file']['tmp_name'],'uploads/'.$name.'_compressed'.'.'.$ext);
 print_r($_FILES);
 ?>
